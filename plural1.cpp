449fe33e17045a3d3e9505666fb959e3474eb78f
omarosamahu <omarosama255@gmail.com>
2019-07-27 10:09:15 +0200

    This is commit for callbacks

--- plural1.cpp
+++ plural1.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+void pass(double  y){
+    y += 3;
+    double x = y + 2 ;
+    cout << x << endl; 
+}
+
+double passByAddr(double &y){
+   
+    return y*2;
+}
+
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    return 0;
+    const int a = 10;
+    int b = 5;
+    const int * p = &a;
+    cout << p << endl;
+    ++p;
+    cout << p << endl;
+    int * const p2 = &b;
+    *p2 = 30;
+    cout << *p2 << endl;
+
+    // double val = passByAddr(pi);
+    // cout << val << endl; 
+    // cout << pi << endl;
+    // int i = static_cast<int>(d);
+    
+    return 0; 
 }
