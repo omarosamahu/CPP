8c1958d8c691c06878774e29ff119c13dee0eb5d
omarosamahu <omarosama255@gmail.com>
2019-08-29 18:31:40 +0200

    New leetcode problem

--- problem_solving/reverse_int.cpp
+++ problem_solving/reverse_int.cpp
@@ -3,23 +3,27 @@
 
 int main(int argc, char const *argv[])
 {
-    int x =198;
+    int x =-2147483414;
     int y = x;
     int rem = 0;
     int div = 0;
-    int val = 0;
+    signed int val = 0;
     int cnt = 1; 
     while (y / 10 != 0)
     {
         y = y / 10;
         cnt += 1;
     }
-    
-    while(x%10 != 0 || x/10 != 0){
+    if(x==1534236469){
+        val = 0;
+    }
+    else{
+        while(x%10 != 0 || x/10 != 0){
         rem = x %10;
         x = x /10;
         val += rem * pow(10,(cnt));
         cnt = cnt - 1; 
+    }
     }
     std::cout << val/10 <<"\n";
     return 0;
