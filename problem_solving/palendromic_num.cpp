7c6af7191f45f77fae8db1eb2a6dd7e4be4e60d3
omarosamahu <omarosama255@gmail.com>
2019-09-06 11:54:32 +0200

    palendrom

---
+++ problem_solving/palendromic_num.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using vect = std::vector<int>;
+
+bool is_palendrom(unsigned n){
+    vect buff;
+    int val;
+    bool check = true;
+    
+    while(n%10 != 0 || n/10 != 0){
+        
+       if(n/10 == 1){
+           buff.push_back(0);
+           n = n/10;
+       }
+       else
+       {
+            val = n%10;
+        n = n/10;
+        buff.push_back(val);
+       }
+       
+        
+        
+        
+    }
+    for(int i=0;i < buff.size();i++){
+        cout << buff[i] << " ";
+    }
+    cout << std::endl;
+    
+    int i = buff.size()-1;
+    int j = 0;
+   
+   
+    while (i >= j)
+    {   
+        
+        if(buff[i] == buff[j]){
+            check = true;
+            j += 1;
+            i -= 1;
+            
+        }
+        else
+        {
+            check = false;
+            break;
+        }
+        
+        
+    }
+
+    
+ return check;   
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    bool chekPal = is_palendrom(1010);
+    cout << chekPal << std::endl;
+    return 0;
+}
